33535e1da966f7820569f709e8c1c8ae0a7aff9f
grumply <sean@grump.ly>
2018-07-04 07:11:30 -0500

    Bump version in default.nix.

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure, pure-css, pure-txt, pure-txt-trie, hashable, stdenv }:
 mkDerivation {
   pname = "pure-theme";
-  version = "0.1.0.0";
+  version = "0.7.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure pure-css pure-txt pure-txt-trie hashable ];
   homepage = "github.com/grumply/pure-theme";
